31e5f7a8f0a412415706b68f3c7b0112cf776a35
r2goyal <r2goyal@gmail.com>
2017-04-21 15:20:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const request = require('request')
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,31 +13,46 @@ restService.post('/webhookToChatbot/', function (req, res) {
     console.log('webhookToChatbot request....................');
 
     try {
-        var speech = 'empty speech';
+//         var speech = 'empty speech';
+         var botResponse = '';
 
         if (req.body) {
-            var requestBody = req.body;
+//             var requestBody = req.body;
 
-            if (requestBody.result) {
-                speech = '';
+//             if (requestBody.result) {
+//                 speech = '';
 
-                if (requestBody.result.fulfillment) {
-                    speech += requestBody.result.fulfillment.speech;
-                    speech += ' ';
-                }
+//                 if (requestBody.result.fulfillment) {
+//                     speech += requestBody.result.fulfillment.speech;
+//                     speech += ' ';
+//                 }
 
-                if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;
-                }
-            }
+//                 if (requestBody.result.action) {
+//                     speech += 'action: ' + requestBody.result.action;
+//                 }
+//             }
+             console.log("Text : "+req.body.result.resolvedQuery);
+            request({
+				url: 'http://72.55.146.142:9091/chatbot/rest/Chatbot/getResponse?request='+req.body.result.resolvedQuery,
+				method: 'GET',
+				async:false
+				}, function(error, response, body) {
+				if (error) {
+					console.log('Error sending messages: ', error)
+				}else{
+					console.log("in else response block............");
+					console.log(response.body);
+					botResponse += response.body;
+				}
+			})
         }
-
-        console.log('webhookToChatbot result: ', speech);
+			
+        console.log('webhookToChatbot result: ', botResponse);
 
         return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'apiai-webhook-sample'
+            speech: botResponse,
+            displayText: botResponse,
+            source: 'r2goyal/apiai_nodejs'
         });
     } catch (err) {
         console.error("Can't process request for webhookToChatbot ", err);
